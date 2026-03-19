5ac3a13f86b46dc3fa0c32a09bd6c29d4a758c19
SeLoPin <jayaxlesalise@gmail.com>
2026-03-19 21:44:29 +0800

    Initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,31 @@
+<<<<<<< HEAD
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+=======
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
@@ -238,3 +266,4 @@ function App() {
 }
 
 export default App;
+>>>>>>> 7a300ca (add something)
